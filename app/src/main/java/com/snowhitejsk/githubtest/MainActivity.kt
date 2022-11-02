a694c448c851f29f74e2830d9ede71cc1c3c28fc
kimjunseok0915 <tommy20020915@gmail.com>
2022-11-02 15:20:33 +0900

    make button view

--- app/src/main/java/com/snowhitejsk/githubtest/MainActivity.kt
+++ app/src/main/java/com/snowhitejsk/githubtest/MainActivity.kt
@@ -2,10 +2,15 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        val btn: Button =findViewById(R.id.btn)
+        btn.setOnClickListener{ Toast.makeText(this, "clicked button", Toast.LENGTH_SHORT).show()}
     }
 }
